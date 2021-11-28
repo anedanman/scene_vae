f8903efb35632aee604231de1f9c76ba961b991a
anedanman <60619274+anedanman@users.noreply.github.com>
2021-11-29 00:42:34 +0300

    perceptual discriminator

--- model.py
+++ model.py
@@ -4,18 +4,8 @@ from torch.nn import functional as F
 from tqdm import tqdm
 import math
 import numpy as np
+from utils import get_rot_matr
 
-
-def get_rot_matr(n, x1, x2, alpha):
-    rad = torch.tensor(alpha * math.pi / 180)
-    s = torch.sin(rad)
-    c = torch.cos(rad)
-    rot = torch.eye(n)
-    rot[x1, x1] = c
-    rot[x2, x2] = c
-    rot[x1, x2] = -s
-    rot[x2, x1] = s
-    return rot
 
 class Residual(nn.Module):
     def __init__(self, in_channels, num_hiddens, num_residual_hiddens):
@@ -49,6 +39,68 @@ class ResidualStack(nn.Module):
         for i in range(self._num_residual_layers):
             x = self._layers[i](x)
         return F.relu(x)
+
+
+class Autoencoder(nn.Module):
+    def __init__(self, hidden_dims=None, in_channels=1):
+        super(Autoencoder, self).__init__()
+        self.in_channels = in_channels
+        self.hidden_dims = [32, 64, 64, 64, 16] if hidden_dims is None else hidden_dims
+        self.build_encoder()
+        self.build_decoder()
+
+    def build_encoder(self):
+        modules = []
+        in_channels = self.in_channels
+        for h_dim in self.hidden_dims:
+            modules.append(
+                nn.Sequential(
+                    nn.Conv2d(in_channels, out_channels=h_dim,
+                              kernel_size=3, stride=2, padding=1),
+                    nn.BatchNorm2d(h_dim),
+                    nn.LeakyReLU())
+            )
+            in_channels = h_dim
+        self.encoder = nn.Sequential(*modules)
+    
+    def build_decoder(self):
+        modules = []
+        in_channels = self.hidden_dims[-1]
+        for h_dim in self.hidden_dims[-2::-1]:
+            modules.append(
+                nn.Sequential(
+                    nn.ConvTranspose2d(in_channels,
+                                       h_dim,
+                                       kernel_size=3,
+                                       stride = 2,
+                                       padding=1,
+                                       output_padding=1),
+                    nn.BatchNorm2d(h_dim),
+                    nn.LeakyReLU())
+            )
+            in_channels = h_dim
+        modules.append(
+            nn.Sequential(
+                nn.ConvTranspose2d(self.hidden_dims[0],
+                                    self.hidden_dims[0],
+                                    kernel_size=3,
+                                    stride=2,
+                                    padding=1,
+                                    output_padding=1),
+                nn.BatchNorm2d(self.hidden_dims[0]),
+                nn.LeakyReLU(),
+                nn.Conv2d(self.hidden_dims[0], out_channels=self.in_channels,
+                          kernel_size= 3, padding= 1),
+                nn.Tanh())
+            )
+        self.decoder = nn.Sequential(*modules)
+
+    def freeze(self):
+        for param in self.parameters():
+            param.requires_grad = False
+
+    def forward(self, x):
+        return self.decoder(self.encoder(x))
 
 
 class SceneVAE(nn.Module):
@@ -188,6 +240,33 @@ class SceneVAE(nn.Module):
         loss = self.discr_loss(pred, ys)
         return loss
 
+    def perceptual(self, zs, encoder_model):
+        mse = torch.nn.MSELoss(reduction='sum')
+        rotated_low = []
+        rotated_high = []
+        for zs_i in zs:
+            xs = np.random.randint(1, self.embed_dim - 1, 2)
+            x1, x2 = min(xs), max(xs)
+            alpha = np.random.randint(3, 13)
+            rot_matr = get_rot_matr(self.embed_dim, x1, x2, alpha).to('cuda')
+            zs_i_rot = zs_i @ rot_matr.t()
+            rotated_low.append(zs_i_rot)
+        for zs_i in zs:
+            xs = np.random.randint(1, self.embed_dim - 1, 2)
+            x1, x2 = min(xs), max(xs)
+            alpha = np.random.randint(75, 105)
+            rot_matr = get_rot_matr(self.embed_dim, x1, x2, alpha).to('cuda')
+            zs_i_rot = zs_i @ rot_matr.t()
+            rotated_high.append(zs_i_rot)
+        zs = self._lin(torch.flatten(zs, 0, 1))
+        rotated_low = self._lin(torch.flatten(torch.stack(rotated_low), 0, 1))
+        rotated_high = self._lin(torch.flatten(torch.stack(rotated_high), 0, 1))
+        zs_perc = encoder_model(self.decoder(zs))
+        zs1_perc = encoder_model(self.decoder(rotated_low))
+        zs2_perc = encoder_model(self.decoder(rotated_high))
+        loss = mse(zs_perc, zs1_perc) - 0.5 * mse(zs_perc, zs2_perc)
+        return loss
+
     def decoder(self, z):
         x = z.view(-1, 1, 32, 32)
         x = self._unpool(x)
@@ -199,90 +278,9 @@ class SceneVAE(nn.Module):
         x = self.final_act(x)
         return x
     
-    def forward(self, inputs,labels):
+    def forward(self, inputs,labels, encoder_model):
         encoded_inputs = self.encoder(inputs)
         z, mus, logvars, zs = self.latent_operations(encoded_inputs, labels)
-        discr_loss = self.discriminate(zs)
-        return self.decoder(z), zs, mus, logvars, discr_loss
-
-
-def loss_func(recon_x, x, mus, logvars):
-    mse_loss = torch.nn.MSELoss(reduction='sum')
-    mse = mse_loss(recon_x, x)
-    kld = -0.5 * torch.sum(1 + logvars - mus.pow(2) - logvars.exp())
-    return mse + kld, mse, kld
-
-
-def train_epoch(model, train_dataloader, loss_func, optimizer, epoch, gamma=1):
-    model.train()
-    train_loss = 0
-    train_mse_loss = 0
-    train_kld_loss = 0
-    train_discr_loss = 0
-    n = len(train_dataloader.dataset)
-    for i, batch in enumerate(tqdm(train_dataloader)):
-        scene = batch['scene'].to('cuda')
-        masks = batch['masks'].to('cuda')
-        labels = batch['labels'].to('cuda')
-        recon_scene, _, mus, logvars, discr_loss = model(masks, labels)
-        loss, mse_loss, kld_loss = loss_func(recon_scene, scene, mus, logvars)
-        loss += discr_loss * gamma
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-        train_loss += loss.item()
-        train_mse_loss += mse_loss.item()
-        train_kld_loss += kld_loss.item()
-        train_discr_loss += discr_loss.item()
-    print('====> Epoch: {} Train loss: {:.4f}, MSE loss: {:.4f}, discr loss: {:.4f}'.format(
-          epoch, train_loss / n, train_mse_loss / n, train_discr_loss / n))
-    return train_loss / n, train_mse_loss / n, train_kld_loss / n, train_discr_loss / n
-
-
-def test_epoch(model, test_dataloader, loss_func, epoch, gamma=1):
-    model.eval()
-    test_loss = 0
-    test_mse_loss = 0
-    test_kld_loss = 0
-    test_discr_loss = 0
-    n = len(test_dataloader.dataset)
-    with torch.no_grad():
-        for i, batch in enumerate(test_dataloader):
-            scene = batch['scene'].to('cuda')
-            masks = batch['masks'].to('cuda')
-            labels = batch['labels'].to('cuda')
-            recon_scene, _, mus, logvars, discr_loss = model(masks, labels)
-            loss, mse_loss, kld_loss = loss_func(recon_scene, scene, mus, logvars)
-            loss += discr_loss * gamma
-            test_loss += loss.item()
-            test_mse_loss += mse_loss.item()
-            test_kld_loss += kld_loss.item()
-            test_discr_loss += discr_loss.item()
-        print('====> Epoch: {} Test loss: {:.4f}, MSE loss: {:.4f}, discr loss: {:.4f}'.format(
-            epoch, test_loss / n, test_mse_loss / n, test_discr_loss / n))
-    return test_loss / n, test_mse_loss / n, test_kld_loss / n, test_discr_loss / n
-
-
-def train_model(model, train_dataloader, test_dataloader, optimizer, num_epochs=20, scheduler=None):
-    min_loss = float('inf')
-    train_losses = []
-    test_losses = []
-    mse_train = []
-    mse_test = []
-    kld_train = []
-    kld_test = []
-    for epoch in range(num_epochs):
-        train_loss, mse_loss_train, kld_loss_train, discr_loss_train = train_epoch(model, train_dataloader, loss_func, optimizer, epoch)
-        test_loss, mse_loss_test, kld_loss_test, discr_loss_test = test_epoch(model, test_dataloader, loss_func, epoch)
-        train_losses.append(train_loss)
-        test_losses.append(test_loss)
-        mse_train.append(mse_loss_train)
-        mse_test.append(mse_loss_test)
-        kld_train.append(kld_loss_train)
-        kld_test.append(kld_loss_test)
-        if scheduler is not None:
-            scheduler.step()
-        if test_loss < min_loss:
-            min_loss = test_loss
-            torch.save(model.state_dict(), 'best_model.pth')
-    return train_losses, test_losses, mse_train, mse_test, kld_train, kld_test
+        perceptual_loss = self.perceptual(zs, encoder_model) 
+        #discr_loss = self.discriminate(zs)
+        return self.decoder(z), zs, mus, logvars, perceptual_loss
